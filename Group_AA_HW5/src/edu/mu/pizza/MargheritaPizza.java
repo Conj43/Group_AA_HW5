2c6d3d7009649923a29e6860c09f2c65ef157e29
Colin Buchheit <130728626+ColinBuchheit@users.noreply.github.com>
2024-03-10 15:56:20 -0500

    Implemented Hawaiian/Margherita

--- Group_AA_HW5/src/edu/mu/pizza/MargheritaPizza.java
+++ Group_AA_HW5/src/edu/mu/pizza/MargheritaPizza.java
@@ -1,55 +1,59 @@
 package edu.mu.pizza;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import edu.mu.Toppings;
 import edu.mu.cooking.ICookingStrategy;
 
-public class MargheritaPizza extends AbstractPizza{
+public class MargheritaPizza extends AbstractPizza {
 
-	//default constructor
-		public MargheritaPizza() {
-		}
+    //default constructor initializes with default toppings
+    public MargheritaPizza() {
+        super();
+        this.priceWithoutToppings = 2.50; //set default price for MargheritaPizza
+        this.toppingList = new ArrayList<>(); //initialize the toppings list
+        //adds default toppings specific to MargheritaPizza
+        toppingList.add(Toppings.TOMATO);
+        toppingList.add(Toppings.CHEESE);
+        //calculates the  initial total price including toppings
+        updatePizzaPrice();
+    }
 
-		
-		//super constructor
-		public MargheritaPizza(List<Toppings> toppingList, double priceWithoutToppings, double totalPrice, int pizzaOrderID,
-				ICookingStrategy cookingStrategy, double cookingPrice) {
-			super(toppingList, priceWithoutToppings, totalPrice, pizzaOrderID, cookingStrategy, cookingPrice);
-		}
-		
-		//copy constructor
-		public MargheritaPizza(HawaiianPizza mPizza) {
-			this.toppingList = mPizza.toppingList;
-			this.priceWithoutToppings = mPizza.priceWithoutToppings;
-			this.totalPrice = mPizza.totalPrice;
-			this.pizzaOrderID = mPizza.pizzaOrderID;
-			this.cookingStrategy = mPizza.cookingStrategy;
-			this.cookingPrice = mPizza.cookingPrice;
-		}
-		
-		
-		
-		//generated toStrong method
-		@Override
-		public String toString() {
-			return "MargheritaPizza [toppingList=" + toppingList + ", priceWithoutToppings=" + priceWithoutToppings
-					+ ", totalPrice=" + totalPrice + ", pizzaOrderID=" + pizzaOrderID + ", cookingStrategy="
-					+ cookingStrategy + ", cookingPrice=" + cookingPrice + "]";
-		}
+    //parameterized constructor for custom orders
+    public MargheritaPizza(List<Toppings> toppingList, double priceWithoutToppings, double totalPrice, int pizzaOrderID,
+                           ICookingStrategy cookingStrategy, double cookingPrice) {
+        super(toppingList, priceWithoutToppings, totalPrice, pizzaOrderID, cookingStrategy, cookingPrice);
+    }
 
+    //copy constructor creates a deep copy of MargheritaPizza
+    public MargheritaPizza(MargheritaPizza mPizza) {
+        super(new ArrayList<>(mPizza.toppingList), mPizza.priceWithoutToppings, mPizza.totalPrice, 
+              mPizza.pizzaOrderID, mPizza.cookingStrategy, mPizza.cookingPrice);
+    }
+    
+    @Override
+    protected double addTopingsToPrice(double priceWithoutToppings) {
+        double toppingsPrice = 0;
+        for (Toppings topping : toppingList) {
+            toppingsPrice += topping.getPrice();
+        }
+        return priceWithoutToppings + toppingsPrice;
+    }
 
-		@Override
-		protected double addTopingsToPrice(double priceWithoutToppings) {
+    @Override
+    public double updatePizzaPrice() {
+        this.totalPrice = addTopingsToPrice(this.priceWithoutToppings);
+        return this.totalPrice;
+    }
 
-			return 0;
-		}
-
-
-		@Override
-		public double updatePizzaPrice() {
-
-			return 0;
-		}
-		
+    @Override
+    public String toString() {
+        return "MargheritaPizza {" +
+                "toppingList=" + toppingList +
+                ", priceWithoutToppings=" + priceWithoutToppings +
+                ", totalPrice=" + totalPrice +
+                ", pizzaOrderID=" + pizzaOrderID +
+                ", cookingStrategy=" + cookingStrategy +
+                ", cookingPrice=" + cookingPrice +
+                '}';
+    }
 }
